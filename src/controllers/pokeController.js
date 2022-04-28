62d343c95eadae5824964ee4b864f9085f1c70ab
Bernardo Souza <bernardombsouza@gmail.com>
2022-04-28 00:02:03 -0300

    config consult pokemons

--- src/controllers/pokeController.js
+++ src/controllers/pokeController.js
@@ -15,4 +15,23 @@ router.post('/register', async (req, res) => {
   }
 })
 
+router.get('', async (req, res) => {
+  var query = {}
+  if (req.query.type1){
+    query.type1 = req.query.type1
+  }
+  if (req.query.name){
+    query.name = req.query.name
+  }
+  if (req.query.type2){
+    query.type2 = req.query.type2
+  }
+  try{
+    const pokemon = await Pokemon.find(query).exec()
+    return res.status(200).json(pokemon)
+  }catch(e){
+    return res.status(400).json({error: e})
+  }
+})
+
 module.exports = app => app.use('/pokemon', router);
