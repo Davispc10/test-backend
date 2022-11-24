5ddfda65097385db6df433221f44c613a54e3530
henricker <henriquevieira@alu.ufc.br>
2022-11-24 12:29:28 -0300

    test: add unit tests to get pokemon use case

---
+++ __tests__/unit/data/usecases/pokemons/db-get-pokemon-use-case.spec.ts
@@ -0,0 +1,48 @@
+import { describe, it } from '@jest/globals';
+import { PokemonMockRepository } from '../../../mocks/repository/pokemon-mock-repository';
+import { DbGetPokemonUseCase } from '../../../../../src/app/data/usecases/pokemons/db-get-pokemon-use-case';
+
+const makeSut = () => {
+  const repository = new PokemonMockRepository();
+  const usecase = new DbGetPokemonUseCase(repository);
+
+  return {
+    repository,
+    usecase,
+  };
+};
+
+describe('# Usecase - get-pokemons', () => {
+  it('Should call repository with correct params', async () => {
+    const { repository, usecase } = makeSut();
+    const getPokemonsSpy = jest.spyOn(repository, 'getPokemons');
+    await usecase.execute({ limit: 1, page: 1 });
+    expect(getPokemonsSpy).toHaveBeenCalledWith({ limit: 1, page: 1 });
+  });
+  it('Should throw if repository throws', async () => {
+    const { repository, usecase } = makeSut();
+    jest.spyOn(repository, 'getPokemons').mockRejectedValueOnce(() => {
+      throw new Error();
+    });
+
+    await expect(usecase.execute({ limit: 1, page: 1 })).rejects.toThrow();
+  });
+  it('Should return correct data', () => {
+    const { usecase } = makeSut();
+    const result = usecase.execute({ limit: 1, page: 1 });
+    expect(result).resolves.toStrictEqual({
+      meta: {
+        total: 1,
+        limit: 1,
+        page: 1,
+      },
+      data: [
+        {
+          id: 1,
+          pokedexNumber: 1,
+          name: 'Bulbasaur',
+        },
+      ],
+    });
+  });
+});
---
+++ __tests__/unit/mocks/repository/pokemon-mock-repository.ts
@@ -0,0 +1,27 @@
+import { IPokemonRepository } from '../../../../src/app/data/repositories/pokemon/pokemon-repository';
+import { IPokemon } from '../../../../src/app/domain/entities/pokemon';
+import { getPokemonOptionsQuery } from '../../../../src/app/domain/usecases/pokemon/get-pokemons-use-case';
+import { PaginationData } from '../../../../src/app/domain/util/pagination-data';
+
+export class PokemonMockRepository implements IPokemonRepository {
+  getPokemons(
+    options: getPokemonOptionsQuery
+  ): Promise<PaginationData<IPokemon>> {
+    return new Promise((resolve) => {
+      resolve({
+        meta: {
+          total: 1,
+          limit: 1,
+          page: 1,
+        },
+        data: [
+          {
+            id: 1,
+            pokedexNumber: 1,
+            name: 'Bulbasaur',
+          },
+        ] as unknown as IPokemon[],
+      });
+    });
+  }
+}
