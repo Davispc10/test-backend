784eb9710b432eab0feb81230a0776e2797b9c0f
henricker <henriquevieira@alu.ufc.br>
2022-11-24 23:06:49 -0300

    feat: should pokemon prisma repository return correct data by correct conditions

---
+++ src/app/infra/db/prisma/pokemon-prisma-repository.ts
@@ -0,0 +1,67 @@
+import { PrismaClient } from '@prisma/client';
+import { IPokemonRepository } from '../../../data/repositories/pokemon/pokemon-repository';
+import { IPokemon } from '../../../domain/entities/pokemon';
+import { getPokemonOptionsQuery } from '../../../domain/usecases/pokemon/get-pokemons-use-case';
+import { PaginationData } from '../../../domain/util/pagination-data';
+
+export class PokemonPrismaRepository implements IPokemonRepository {
+  constructor(private readonly connection: PrismaClient) {}
+
+  async getPokemons(
+    options: getPokemonOptionsQuery
+  ): Promise<PaginationData<IPokemon>> {
+    const where = {
+      name: options?.name,
+      pokemonEvolutionInfo: {
+        evolutionStage: options?.evolutionStage,
+        envolved: options?.evolved,
+        familyId: options?.familyId,
+      },
+      type: {
+        some: {
+          name: options?.type,
+        },
+      },
+      weather: {
+        some: {
+          name: options?.weather,
+        },
+      },
+    };
+
+    const data = await this.connection.pokemon.findMany({
+      where,
+      include: {
+        powerStatus: true,
+        pokemonEvolutionInfo: true,
+        pokemonCharacteristics: true,
+        type: {
+          select: {
+            name: true,
+          },
+        },
+        weather: {
+          select: {
+            name: true,
+          },
+        },
+      },
+      take: options.limit,
+      skip: (options.page - 1) * options.limit,
+    });
+
+    const total = await this.connection.pokemon.count({
+      where,
+    });
+
+    return {
+      data,
+      meta: {
+        total,
+        limit: options.limit,
+        page: options.page,
+        hasNext: total > options.limit * options.page,
+      },
+    };
+  }
+}
