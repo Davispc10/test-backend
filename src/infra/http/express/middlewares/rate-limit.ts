16829b4df54046178d1e412fd68f20ee148850be
henricker <henriquevieira@alu.ufc.br>
2022-11-27 12:33:56 -0300

    refactor: ajust comment to 1 min of rate limit

--- src/infra/http/express/middlewares/rate-limit.ts
+++ src/infra/http/express/middlewares/rate-limit.ts
@@ -2,7 +2,7 @@ import rateLimit from 'express-rate-limit';
 
 // One minute => 100 requests
 export const rateLimitRequests = rateLimit({
-  windowMs: 60000, // 1 hour
+  windowMs: 60000, // 1 min
   max: 100, // limit each IP to 100 requests per windowMs
   handler: (_, res) => {
     res.status(429).json({
