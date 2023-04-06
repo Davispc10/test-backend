c5136191e17ae01131bafb3348a1ada7898f677e
Mariana Bastos <maribastossrodrigues@gmail.com>
2023-04-06 18:13:02 -0300

    chore: add prisma, string connection and model

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,15 @@
 import bodyParser from 'body-parser';
 import express, { Application } from 'express';
+import XLSX from 'xlsx';
 
 const app: Application = express();
 
 app.use(bodyParser.json());
+
+const filePath = 'data/Pokemon Go.xlsx';
+const workbook = XLSX.readFile(filePath);
+const worksheet = workbook.Sheets[workbook.SheetNames[0]];
+const data = XLSX.utils.sheet_to_json(worksheet);
+console.log(data);
 
 export { app };
