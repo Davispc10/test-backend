734bda7a3e914afa9f21630837e738326b94865f
Silas Augusto <augusto.fernandes775@gmail.com>
2023-04-10 00:00:11 -0300

    feat: validate uuid in pokemon search by name or id

--- src/main/factories/find-pokemon-by-name-or-id-service.factory.ts
+++ src/main/factories/find-pokemon-by-name-or-id-service.factory.ts
@@ -1,6 +1,7 @@
 import { PokemonRepository } from "../../infra/pokemon.repository"
+import { UuidAdapter } from "../../infra/uuid-adapter"
 import { FindPokemonByNameOrIdDb } from "../../services/find-pokemon-by-name-or-id.service"
 
 export const makeFindPokemonByNameOrIdService = () => {
-  return new FindPokemonByNameOrIdDb(new PokemonRepository(), new PokemonRepository())
+  return new FindPokemonByNameOrIdDb(new PokemonRepository(), new PokemonRepository(), new UuidAdapter())
 }
--- src/services/find-pokemon-by-name-or-id.service.ts
+++ src/services/find-pokemon-by-name-or-id.service.ts
@@ -1,3 +1,5 @@
+import { UuidValidator } from "../domain/contracts/uuid-validator.contract";
+import { InvalidDataException } from "../domain/errors/invalid-data-exception";
 import { FindPokemonByIdRepository } from "../domain/repositories/find-pokemon-by-id.repository";
 import { FindPokemonByNameRepository } from "../domain/repositories/find-pokemon-by-name.repository";
 import { FindPokemonByNameOrIdService } from "../domain/services/find-pokemon-by-name-or-id.service";
@@ -6,13 +8,16 @@ export class FindPokemonByNameOrIdDb implements FindPokemonByNameOrIdService {
   constructor(
     private readonly findPokemonByNameRepository: FindPokemonByNameRepository,
     private readonly findPokemonByIdRepository: FindPokemonByIdRepository,
+    private readonly uuidValidator: UuidValidator
   ) { }
 
   async findByNameOrId({ name, id }: FindPokemonByNameOrIdService.Param): Promise<FindPokemonByNameOrIdService.Result> {
-    if (!name && !id) throw new Error('Missing field')
+    if (!name && !id) throw new InvalidDataException('Missing field')
 
-    if (id && !name)
+    if (id && !name) {
+      if (!this.uuidValidator.isValid(id)) throw new InvalidDataException('Invalid id')
       return await this.findPokemonByIdRepository.findById({ id })
+    }
     if (name && !id)
       return await this.findPokemonByNameRepository.findByName({ name })
   }
