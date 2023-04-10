e4bf3698c907eeda15c2eacf7a33a662f3905c57
Silas Augusto <augusto.fernandes775@gmail.com>
2023-04-09 23:09:55 -0300

    feat: allow GET /pokemons/search?{{name}}&{{id}} route

--- src/presentation/pokemon.routes.ts
+++ src/presentation/pokemon.routes.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express'
 import { makeFindAllPokemonsController } from '../main/factories/find-all-pokemons-controller.factory'
+import { makeFindPokemonByNameOrIdController } from '../main/factories/find-pokemon-by-name-or-id-controller.factory'
 
 const pokemonRouters = Router()
 
 pokemonRouters.get('/pokemons', makeFindAllPokemonsController().findAll)
+pokemonRouters.get('/pokemons/search', makeFindPokemonByNameOrIdController().find)
 
 export { pokemonRouters }
