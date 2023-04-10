b9fbfbe9223627c83411922d461133e5e55ca126
Mariana Bastos <maribastossrodrigues@gmail.com>
2023-04-09 22:35:13 -0300

    refactor: change routes in app to router file

--- src/app.ts
+++ src/app.ts
@@ -1,70 +1,11 @@
-import { pokemonMapper } from './application/pokemon/helpers/mapper';
-import { Pokemon } from 'application/pokemon/interfaces/pokemon';
+import router from './application/pokemon/routes/router';
 import bodyParser from 'body-parser';
-import express, { Application, Request, Response, Router } from 'express';
-import xlsx from 'xlsx';
+import express, { Application } from 'express';
 
 const app: Application = express();
-const router = Router();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/api', router);
-
-const filePath = 'data/Pokemon Go.xlsx';
-const file = xlsx.readFile(filePath, { type: 'buffer' });
-const sheetName = file.SheetNames[0];
-const worksheet = file.Sheets[sheetName];
-const data: Pokemon[] = xlsx.utils.sheet_to_json(worksheet);
-const mappedData = pokemonMapper(data);
-
-router.get('/pokemon', (req: Request, res: Response) => {
-  try {
-    res.status(200).send(mappedData);
-  } catch (error) {
-    console.error(error);
-  }
-});
-
-router.get('/pokemon/search', (req: Request, res: Response) => {
-  try {
-    const query = req.query.q as string;
-    const results = mappedData.filter((p: Pokemon) => {
-      return p.name.includes(query);
-    });
-
-    res.status(200).send(results);
-  } catch (error) {
-    console.error(error);
-  }
-});
-
-router.get('/pokemon/page/:pageNumber', (req: Request, res: Response) => {
-  try {
-    const pageSize = 10;
-    const pageNumber = parseInt(req.params.pageNumber, 10);
-    const startIndex = (pageNumber - 1) * pageSize;
-    const endIndex = startIndex + pageSize;
-    const results = mappedData.slice(startIndex, endIndex);
-
-    res.status(200).send(results);
-  } catch (error) {
-    console.error(error);
-  }
-});
-
-router.get('/pokemon/filter/:field/:value', (req: Request, res: Response) => {
-  try {
-    const field = req.params.field;
-    const value = req.params.value;
-    const filteredData = mappedData.filter((item: any) => {
-      return item[field] === value;
-    });
-
-    res.status(200).send(filteredData);
-  } catch (error) {
-    console.error(error);
-  }
-});
 
 export { app };
---
+++ src/application/pokemon/routes/router.ts
@@ -0,0 +1,63 @@
+import { Request, Response, Router } from 'express';
+import { Pokemon } from '../interfaces/pokemon';
+import { pokemonMapper } from '../helpers/mapper';
+import xlsx from 'xlsx';
+
+const router = Router();
+const filePath = 'data/Pokemon Go.xlsx';
+const file = xlsx.readFile(filePath, { type: 'buffer' });
+const sheetName = file.SheetNames[0];
+const worksheet = file.Sheets[sheetName];
+const data: Pokemon[] = xlsx.utils.sheet_to_json(worksheet);
+const mappedData = pokemonMapper(data);
+
+router.get('/pokemon', (req: Request, res: Response) => {
+  try {
+    res.status(200).send(mappedData);
+  } catch (error) {
+    console.error(error);
+  }
+});
+
+router.get('/pokemon/search', (req: Request, res: Response) => {
+  try {
+    const query = req.query.q as string;
+    const results = mappedData.filter((p: Pokemon) => {
+      return p.name.includes(query);
+    });
+
+    res.status(200).send(results);
+  } catch (error) {
+    console.error(error);
+  }
+});
+
+router.get('/pokemon/page/:pageNumber', (req: Request, res: Response) => {
+  try {
+    const pageSize = 10;
+    const pageNumber = parseInt(req.params.pageNumber, 10);
+    const startIndex = (pageNumber - 1) * pageSize;
+    const endIndex = startIndex + pageSize;
+    const results = mappedData.slice(startIndex, endIndex);
+
+    res.status(200).send(results);
+  } catch (error) {
+    console.error(error);
+  }
+});
+
+router.get('/pokemon/filter/:field/:value', (req: Request, res: Response) => {
+  try {
+    const field = req.params.field;
+    const value = req.params.value;
+    const filteredData = mappedData.filter((item: any) => {
+      return item[field] === value;
+    });
+
+    res.status(200).send(filteredData);
+  } catch (error) {
+    console.error(error);
+  }
+});
+
+export default router;
