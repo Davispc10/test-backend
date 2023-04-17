6b014487f9e0eff56359d5bcff0c92c8d5cc33f6
Ana Laura <88839593+a-mendes@users.noreply.github.com>
2023-04-17 18:38:48 -0300

    fix: always give a response

--- app/controllers/pokemonController.js
+++ app/controllers/pokemonController.js
@@ -1,42 +1,62 @@
 const pokemonModel = require('../models/pokemonModel');
 
 const getAll = async (req, res) => {
-    const pokemons = await pokemonModel.getAll();
-    res.status(200).json(pokemons);
+    try {
+        const pokemons = await pokemonModel.getAll();
+        res.status(200).json(pokemons);
+    } catch(error) {
+        res.status(500).send(error);
+    }
 };
 
 const getByPokedexNumber = async (req, res) => {
-    const pokemons = await pokemonModel.getByPokedexNumber(req.body);
-    res.status(200).json(pokemons);
+    try { 
+        const pokemons = await pokemonModel.getByPokedexNumber(req.body);
+        res.status(200).json(pokemons);
+    } catch(error) {
+        res.status(500).send(error);
+    }
 };
 
 const getByGeneration = async (req, res) => {
-    let {page} = req.query;
-    if(page) {
-        const pokemons = await pokemonModel.getByGenerationPaged(req.body, page);
-        res.status(200).json(pokemons);
-        return;
-    }
+    try {
+        let {page} = req.query;
+        if(page) {
+            const pokemons = await pokemonModel.getByGenerationPaged(req.body, page);
+            res.status(200).json(pokemons);
+            return;
+        }
 
-    const pokemons = await pokemonModel.getByGeneration(req.body);
-    res.status(200).json(pokemons);
+        const pokemons = await pokemonModel.getByGeneration(req.body);
+        res.status(200).json(pokemons);
+    } catch(error) {
+        res.status(500).send(error);
+    }
 };
 
 const getByType = async (req, res) => {
-    let {page} = req.query;
-    if(page) {
-        const pokemons = await pokemonModel.getbyTypePaged(req.body, page);
-        res.status(200).json(pokemons);
-        return;
-    }
+    try { 
+        let {page} = req.query;
+        if(page) {
+            const pokemons = await pokemonModel.getbyTypePaged(req.body, page);
+            res.status(200).json(pokemons);
+            return;
+        }
 
-    const pokemons = await pokemonModel.getByType(req.body);
-    res.status(200).json(pokemons);
+        const pokemons = await pokemonModel.getByType(req.body);
+        res.status(200).json(pokemons);
+    } catch(error) {
+        res.status(500).send(error);
+    }
 };
 
 const getBiggestStatTotal = async (req, res) => {
-    const pokemons = await pokemonModel.getBiggestStatTotal();
-    res.status(200).json(pokemons);
+    try {
+        const pokemons = await pokemonModel.getBiggestStatTotal();
+        res.status(200).json(pokemons);
+    } catch(error) {
+        res.status(500).send(error);
+    }
 }; 
 
 module.exports = {
