3b18d506e8ee7321815c3b3b62102893a74f22db
Eduardo Brito Vaillant <eduardo.bvaillant@gmail.com>
2023-05-07 17:47:09 -0300

    feat: ensure that find pokemon by id repository is implemented correctly

--- src/infra/db/repositories/pokemon.repository.ts
+++ src/infra/db/repositories/pokemon.repository.ts
@@ -1,15 +1,23 @@
 import { type PgPokemon } from '@/infra/db/entities'
-import { type ListPokemonRepository } from '@/domain/contracts/repositories'
-import { type PokemonEntity } from '@/domain/entities'
+import { type FindPokemonByIdRepository, type ListPokemonRepository } from '@/domain/contracts/repositories'
+import { PokemonEntity } from '@/domain/entities'
 
 import { type Repository } from 'typeorm'
 
-export class PokemonRepository implements ListPokemonRepository {
+export class PokemonRepository implements ListPokemonRepository, FindPokemonByIdRepository {
   constructor (
     private readonly pokemonRepository: Repository<PgPokemon>
   ) {}
 
   async list (): Promise<PokemonEntity[]> {
-    return await this.pokemonRepository.find()
+    const dbPokemons = await this.pokemonRepository.find()
+    if (dbPokemons.length === 0) return []
+    return dbPokemons.map(dbPokemon => PokemonEntity.fromDB(dbPokemon))
+  }
+
+  async find (id: number): Promise<PokemonEntity | undefined> {
+    const dbPokemon = await this.pokemonRepository.findOne({ where: { id } })
+    if (!dbPokemon) return undefined
+    return PokemonEntity.fromDB(dbPokemon)
   }
 }
