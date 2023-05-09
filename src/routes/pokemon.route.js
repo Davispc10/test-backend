62df18f37cfa88db9a1b8adb1edd97b2e94d1f84
Felipe Oliveira <felpd3v@gmail.com>
2023-05-09 10:52:35 -0300

    feat: create a default GET route to get and filter pokemons

---
+++ src/routes/pokemon.route.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const pokemonController = require('../controllers/pokemon.controller')
+const { getPaginationInfo } = require('../utils/routing')
+
+const router = express.Router()
+
+router.get('/', async (req, res) => {
+	try {
+		const { page, params, limit, startIndex } = getPaginationInfo(req.query)
+
+		const { count, pokemons } = await pokemonController.getPokemons({
+			startIndex,
+			limit,
+			query: params,
+		})
+
+		res.json({
+			totalItems: count,
+			page,
+			limit,
+			data: pokemons,
+		})
+	} catch (err) {
+		res
+			.status(404)
+			.json({ message: 'It was not possible to find pokemons!', errors: [err] })
+	}
+})
+
+module.exports = router
