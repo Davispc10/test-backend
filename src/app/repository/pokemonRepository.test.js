110c0ba7469d92de221b9e5789bdb41d1ed97a1b
Kevin Siani <sianikevin@gmail.com>
2023-07-29 22:58:01 -0300

    update pokemonRepository tests

--- src/app/repository/pokemonRepository.test.js
+++ src/app/repository/pokemonRepository.test.js
@@ -4,15 +4,38 @@ const PokemonModel = require('../models/pokemon');
 const PokemonRepository = require('./pokemonRepository');
 const pokemonMockPayload = require('../../utils/pokemonMockPayload.json');
 
-describe('pokemonRepository test suite', () => {
-  it('should retrieve a list of pokemons from database', async () => {
-    const stub = sinon.stub(PokemonModel, 'findAndCountAll').returns(pokemonMockPayload);
-    const pokemonRepository = new PokemonRepository();
-    const pokemons = await pokemonRepository.findAll(1, 5);
+const stub = sinon.stub(PokemonModel, 'findAndCountAll').returns(pokemonMockPayload);
+const pokemonRepository = new PokemonRepository();
 
-    expect(stub.calledOnce).toBe(true);
-    expect(pokemons.length).toBe(2);
-    expect(pokemons[0]).toEqual(pokemonMockPayload[0]);
-    expect(pokemons[1]).toEqual(pokemonMockPayload[1]);
+describe('pokemonRepository test suite', () => {
+  describe('findAll', () => {
+    it('should retrieve a list of pokemons from database', async () => {
+      const pokemons = await pokemonRepository.findAll(1, 5);
+  
+      expect(stub.callCount).toBe(1);
+      expect(pokemons.length).toBe(2);
+      expect(pokemons[0]).toEqual(pokemonMockPayload[0]);
+      expect(pokemons[1]).toEqual(pokemonMockPayload[1]);
+    });
+
+    it('should retrieve a list of pokemons from database with filter', async () => {
+      const pokemons = await pokemonRepository.findAll(1, 5, { evolved: true });
+  
+      expect(stub.callCount).toBe(2);
+      expect(pokemons.length).toBe(2);
+      expect(pokemons[0]).toEqual(pokemonMockPayload[0]);
+      expect(pokemons[1]).toEqual(pokemonMockPayload[1]);
+    });
+  });
+
+  describe('findById', () => {
+    it('should retrieve pokemon by ID', async () => {
+      const stub2 = sinon.stub(PokemonModel, 'findOne').returns(pokemonMockPayload[0]);
+      const pokemonRepository2 = new PokemonRepository();
+      const pokemon = await pokemonRepository2.findById(1);
+  
+      expect(stub2.calledOnce).toBe(true);
+      expect(pokemon).toEqual(pokemonMockPayload[0]);
+    });
   });
 });
