df4d3a3d09ea0b418b30ee10000cfe8ad49d5c8b
Kevin Siani <sianikevin@gmail.com>
2023-07-29 21:59:39 -0300

    add routes to express config file

--- src/config/express.js
+++ src/config/express.js
@@ -1,9 +1,19 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const PokemonController = require('../app/controllers/pokemonController');
+const PokemonUseCases = require('../app/useCases/PokemonUseCases');
+const PokemonRepository = require('../app/repository/pokemonRepository');
+
+const pokemonRepository = new PokemonRepository();
+const pokemonUseCases = new PokemonUseCases(pokemonRepository);
+const pokemonController = new PokemonController(pokemonUseCases);
 
 const app = express();
 
 app.use(bodyParser.json());
 
+app.get('/pokemons', pokemonController.getAll);
+app.get('/pokemons/:pokemonId', pokemonController.getByPomekonId);
 
 app.use((err, req, res, next) => {
   console.error(err.stack);
