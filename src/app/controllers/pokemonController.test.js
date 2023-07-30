7da86c03dac29a553b5067d7f36936a951de83db
Kevin Siani <sianikevin@gmail.com>
2023-07-30 10:25:16 -0300

    run prettier

--- src/app/controllers/pokemonController.test.js
+++ src/app/controllers/pokemonController.test.js
@@ -13,77 +13,79 @@ describe('PokemonController test suite', () => {
       res = { json, status };
       status.returns(res);
     });
-  
+
     it('should return 400 bad request if page param is not provided', async () => {
       const req = { query: { pageSize: 1 } };
-  
+
       await new PokemonController().getAll(req, res);
       expect(status.args[0][0]).toBe(400);
       expect(json.args[0][0].error).toBe('"page" is required');
     });
-  
+
     it('should return 400 bad request if pageSize param is not provided', async () => {
       const req = { query: { page: 1 } };
-  
+
       await new PokemonController().getAll(req, res);
       expect(status.args[0][0]).toBe(400);
       expect(json.args[0][0].error).toBe('"pageSize" is required');
     });
-  
+
     it('should return 400 bad request if page param is invalid', async () => {
       const req = { query: { page: 'dfsdf', pageSize: 5 } };
-  
+
       await new PokemonController().getAll(req, res);
       expect(status.args[0][0]).toBe(400);
       expect(json.args[0][0].error).toBe('"page" must be a number');
     });
-  
+
     it('should return 400 bad request if page param is invalid', async () => {
       const req = { query: { page: 1, pageSize: 'teste' } };
-  
+
       await new PokemonController().getAll(req, res);
       expect(status.args[0][0]).toBe(400);
       expect(json.args[0][0].error).toBe('"pageSize" must be a number');
     });
-  
+
     it('should return 400 bad request if page param is smaller than 1', async () => {
       const req = { query: { page: 0, pageSize: 5 } };
-  
+
       await new PokemonController().getAll(req, res);
       expect(status.args[0][0]).toBe(400);
       expect(json.args[0][0].error).toBe(
         '"page" must be greater than or equal to 1'
       );
     });
-  
+
     it('should return 400 bad request if page param is smaller than 1', async () => {
       const req = { query: { page: 2, pageSize: 0 } };
-  
+
       await new PokemonController().getAll(req, res);
       expect(status.args[0][0]).toBe(400);
       expect(json.args[0][0].error).toBe(
         '"pageSize" must be greater than or equal to 1'
       );
     });
-  
+
     it('should return 400 bad request if a non permited query param is provided', async () => {
       const req = { query: { page: 1, pageSize: 5, notPermitedParam: 1 } };
-  
+
       await new PokemonController().getAll(req, res);
       expect(status.args[0][0]).toBe(400);
       expect(json.args[0][0].error).toBe('"notPermitedParam" is not allowed');
     });
-  
+
     it('should return 200 with payload', async () => {
       const req = { query: { page: 1, pageSize: 5 } };
-  
+
       const pokemonRepository = sinon.spy();
       const pokemonListUseCase = new PokemonUseCases(pokemonRepository);
-      const stub = sinon.stub(pokemonListUseCase, 'findAll').returns(pokemonMockPayload);
+      const stub = sinon
+        .stub(pokemonListUseCase, 'findAll')
+        .returns(pokemonMockPayload);
       const pokemonController = new PokemonController(pokemonListUseCase);
-  
+
       await pokemonController.getAll(req, res);
-  
+
       expect(stub.calledOnce).toBe(true);
       expect(status.args[0][0]).toBe(200);
     });
@@ -98,10 +100,10 @@ describe('PokemonController test suite', () => {
       res = { json, status };
       status.returns(res);
     });
-  
+
     it('should return 400 bad request if pokemonId param is not provided', async () => {
       const req = { params: { invalidParam: 1 } };
-  
+
       await new PokemonController().getByPomekonId(req, res);
       expect(status.args[0][0]).toBe(400);
       expect(json.args[0][0].error).toBe('"pokemonId" is required');
@@ -109,7 +111,7 @@ describe('PokemonController test suite', () => {
 
     it('should return 400 bad request if pokemonId param is invalid', async () => {
       const req = { params: { pokemonId: 'invalid' } };
-  
+
       await new PokemonController().getByPomekonId(req, res);
       expect(status.args[0][0]).toBe(400);
       expect(json.args[0][0].error).toBe('"pokemonId" must be a number');
@@ -117,14 +119,16 @@ describe('PokemonController test suite', () => {
 
     it('should return 200 with payload', async () => {
       const req = { params: { pokemonId: 1 } };
-  
+
       const pokemonRepository = sinon.spy();
       const pokemonListUseCase = new PokemonUseCases(pokemonRepository);
-      const stub = sinon.stub(pokemonListUseCase, 'findById').returns(pokemonMockPayload[0]);
+      const stub = sinon
+        .stub(pokemonListUseCase, 'findById')
+        .returns(pokemonMockPayload[0]);
       const pokemonController = new PokemonController(pokemonListUseCase);
-  
+
       await pokemonController.getByPomekonId(req, res);
-  
+
       expect(stub.calledOnce).toBe(true);
       expect(status.args[0][0]).toBe(200);
     });
--- src/server.js
+++ src/server.js
@@ -2,7 +2,6 @@ const expressApp = require('./config/express');
 
 const SERVER_PORT = process.env.SERVER_PORT || 3000;
 
-
 expressApp.listen(SERVER_PORT, () => {
   console.log(`Server listening on ${SERVER_PORT}`);
 });
