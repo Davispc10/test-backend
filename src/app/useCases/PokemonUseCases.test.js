9e2dd5c2f204ca2beef06e00917891685383bc1f
Kevin Siani <sianikevin@gmail.com>
2023-07-29 22:21:34 -0300

    use pokemon mock response in usecase test

--- src/app/useCases/PokemonUseCases.test.js
+++ src/app/useCases/PokemonUseCases.test.js
@@ -2,34 +2,18 @@ const sinon = require('sinon');
 
 const PokemonRepository = require('../repository/pokemonRepository');
 const PokemonUseCases = require('./PokemonUseCases');
-
-const stubValue = [
-  {
-    id: 6,
-    name: 'Charizard',
-    pokedexNumber: 6,
-  },
-  {
-    id: 7,
-    name: 'Squirtle',
-    pokedexNumber: 7,
-  },
-];
+const pokemonMockPayload = require('../../utils/pokemonMockPayload.json');
 
 describe('ListPokemon use case test suite', () => {
   it('should retrieve pokemons from database', async () => {
     const pokemonRepository = new PokemonRepository();
-    const stub = sinon.stub(pokemonRepository, 'findAll').returns(stubValue);
+    const stub = sinon.stub(pokemonRepository, 'findAll').returns(pokemonMockPayload);
     const pokemonListUseCase = new PokemonUseCases(pokemonRepository);
     const pokemons = await pokemonListUseCase.findAll(1, 5);
 
     expect(stub.calledOnce).toBe(true);
     expect(pokemons.length).toBe(2);
-    expect(pokemons[0].id).toBe(6);
-    expect(pokemons[0].name).toBe('Charizard');
-    expect(pokemons[0].pokedexNumber).toBe(6);
-    expect(pokemons[1].id).toBe(7);
-    expect(pokemons[1].name).toBe('Squirtle');
-    expect(pokemons[1].pokedexNumber).toBe(7);
+    expect(pokemons[0]).toEqual(pokemonMockPayload[0]);
+    expect(pokemons[1]).toEqual(pokemonMockPayload[1]);
   });
 });
